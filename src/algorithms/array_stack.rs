0fd72595026e37c4259c8b550526513ff6405350
sebashack <sebastian0092@gmail.com>
2020-03-08 11:40:24 -0500

    Implement iterators for the array stack
    I had to cheat in order to implement IterMut for ArrayStack as it
    turned out to be very tricky to do it with `&'a mut`.

--- src/algorithms/array_stack.rs
+++ src/algorithms/array_stack.rs
@@ -1,7 +1,18 @@
+use std::slice::{IterMut as IterMutVec};
+
 pub struct ArrayStack<T> {
     container: Vec<Option<T>>,
     current: usize,
 }
+
+pub struct ConsumerIter<T>(ArrayStack<T>);
+
+pub struct Iter<'a, T> {
+    container: &'a Vec<Option<T>>,
+    next: usize,
+}
+
+pub struct IterMut<'a, T>(IterMutVec<'a, Option<T>>);
 
 impl<T> ArrayStack<T> {
     pub fn new() -> Self {
@@ -44,6 +55,21 @@ impl<T> ArrayStack<T> {
         }
     }
 
+    pub fn into_iter(self) -> ConsumerIter<T> {
+        ConsumerIter(self)
+    }
+
+    pub fn iter(&self) -> Iter<'_, T> {
+        Iter {
+            container: &self.container,
+            next: self.current,
+        }
+    }
+
+    pub fn iter_mut(&mut self) -> IterMut<'_, T> {
+        IterMut(self.container.iter_mut())
+    }
+
     fn resize(&mut self, capacity: usize) {
         let mut new_container = Vec::with_capacity(capacity);
 
@@ -60,6 +86,38 @@ fn insert<T>(v: &mut Vec<Option<T>>, i: usize, item: Option<T>) {
         v.insert(i, item);
     } else {
         v[i] = item;
+    }
+}
+
+impl<T> Iterator for ConsumerIter<T> {
+    type Item = T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.0.pop()
+    }
+}
+
+impl<'a, T> Iterator for Iter<'a, T> {
+    type Item = &'a T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.next == 0 {
+            None
+        } else {
+            self.next -= 1;
+            self.container[self.next].as_ref()
+        }
+    }
+}
+
+impl<'a, T> Iterator for IterMut<'a, T> {
+    type Item = &'a mut T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        match self.0.next() {
+            None => None,
+            Some(item) => item.as_mut()
+        }
     }
 }
 
@@ -104,5 +162,71 @@ mod tests {
         }
 
         assert!(stack.is_empty());
+    }
+
+    #[test]
+    fn into_iter_should_consume_the_stack() {
+        let mut stack = ArrayStack::<u32>::new();
+
+        for i in 0..25 {
+            stack.push(i);
+        }
+
+        let mut k = 25;
+
+        for v in stack.into_iter() {
+            k -= 1;
+
+            assert!(v == k);
+        }
+
+        //At this point it is impossible to use the stack since it has been moved.
+    }
+
+    #[test]
+    fn iter_should_traverse_the_stack_without_consuming_it() {
+        let mut stack = ArrayStack::<u32>::new();
+
+        for i in 0..25 {
+            stack.push(i);
+        }
+
+        let mut k = 25;
+
+        for v in stack.iter() {
+            k -= 1;
+
+            assert!(*v == k);
+        }
+
+        k = 25;
+
+        //After this point it is totally possible to keep using our stack since
+        //our iterator is just borrowing immutable references.
+
+        assert!(!stack.is_empty());
+
+        for v in stack.iter() {
+            k -= 1;
+
+            assert!(*v == k);
+        }
+    }
+
+    #[test]
+    fn iter_should_traverse_the_stack_while_mutating_it() {
+        let mut stack = ArrayStack::<u32>::new();
+
+        for i in 1..26 {
+            stack.push(i);
+        }
+
+        for v in stack.iter_mut() {
+            *v = 0;
+        }
+
+        for v in stack.iter() {
+            assert!(*v == 0);
+        }
     }
 }
