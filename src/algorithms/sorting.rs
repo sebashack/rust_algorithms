291ad75cbcc412890a23b74fcdaa0b631ff54380
sebashack <sebastian0092@gmail.com>
2020-03-09 20:20:19 -0500

    Implement insertion sort algorithm

--- src/algorithms/sorting.rs
+++ src/algorithms/sorting.rs
@@ -27,15 +27,60 @@ pub fn selection_sort<T>(v: &mut Vec<T>) -> &mut Vec<T>
     selection_sort_by(v, |v0, v1| v0.cmp(v1))
 }
 
+pub fn insertion_sort_by<T, F>(v: &mut Vec<T>, compare: F) -> &mut Vec<T>
+    where F: Fn(&T, &T) -> Ordering
+{
+    let len = v.len();
+
+    for i in 0..len {
+        let mut j = i;
+
+        while j > 0 {
+            if let Less = compare(&v[j], &v[j-1]) {
+                v.swap(j, j-1);
+            } else {
+                break;
+            }
+
+            j -= 1;
+        }
+    }
+
+    v
+}
+
+pub fn insertion_sort<T>(v: &mut Vec<T>) -> &mut Vec<T>
+    where T: Ord
+{
+    insertion_sort_by(v, |v0, v1| v0.cmp(v1))
+}
+
 #[cfg(test)]
 mod tests {
-    use crate::algorithms::sorting::selection_sort_by;
+    use crate::algorithms::sorting::{insertion_sort_by, selection_sort_by};
 
     #[test]
     fn selection_sort_by_should_sort_the_vector() {
-        let mut v = vec![12, 0, 3, 12, 23, 1, 9, 9, 10, 45, 6];
+        let mut v =
+            vec![12, 0, 3, 12, 23, 1, 9, 9, 10, 45, 6, 12, 100, 45, 3, 1, 2];
 
         selection_sort_by(&mut v, |n, m| n.cmp(m));
+
+        let len = v.len();
+
+        for (i, n) in v.iter().enumerate() {
+            if i < len - 1 {
+                assert!(n <= &v[i + 1]);
+            }
+        }
+    }
+
+    #[test]
+    fn insertion_sort_by_should_sort_the_vector() {
+        let mut v =
+            vec![12, 0, 3, 12, 23, 1, 9, 9, 10, 45, 6, 12, 100, 45, 3, 1, 2];
+
+        insertion_sort_by(&mut v, |n, m| n.cmp(m));
 
         let len = v.len();
 
--- src/main.rs
+++ src/main.rs
@@ -5,10 +5,5 @@ use rust_algorithms::algorithms::linked_list_stack;
 use rust_algorithms::algorithms::array_stack;
 
 fn main() {
-    let mut v = vec![1, 2, 3, 4, 5];
-
-    v[0] = 100;
-    v[1] = 103;
-
-    println!("Hello, world! {}", v[1]);
+    println!("{}", 1);
 }
