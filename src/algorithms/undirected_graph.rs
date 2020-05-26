79cfc0b41b1c2ba81c9d7f5372acae156c1ea69d
sebashack <sebastian0092@gmail.com>
2020-05-26 16:11:42 -0500

    Implement breath first search

--- src/algorithms/undirected_graph.rs
+++ src/algorithms/undirected_graph.rs
@@ -1,3 +1,4 @@
+use crate::algorithms::linked_list_queue::LinkedQueue;
 use crate::algorithms::linked_list_stack::LinkedStack;
 
 struct UGraph {
@@ -6,6 +7,12 @@ struct UGraph {
 }
 
 struct DFSPaths {
+    marked: Vec<bool>,
+    edge_to: Vec<Option<usize>>,
+    origin: usize,
+}
+
+struct BFSPaths {
     marked: Vec<bool>,
     edge_to: Vec<Option<usize>>,
     origin: usize,
@@ -91,13 +98,73 @@ impl DFSPaths {
     }
 }
 
+impl BFSPaths {
+    pub fn new(g: &UGraph, origin: usize) -> Self {
+        let mut paths = BFSPaths {
+            marked: Vec::new(),
+            edge_to: Vec::new(),
+            origin,
+        };
+
+        for v in 0..g.num_vertices {
+            paths.marked.push(false);
+            paths.edge_to.push(None);
+        }
+
+        paths.bfs(g, origin);
+
+        paths
+    }
+
+    pub fn has_path_to(&self, v: usize) -> bool {
+        self.marked[v]
+    }
+
+    pub fn path_to(&self, v: usize) -> Option<LinkedStack<usize>> {
+        if self.has_path_to(v) {
+            let mut path = LinkedStack::new();
+
+            let mut x = self.edge_to[v].unwrap();
+            while x != self.origin {
+                path.push(x);
+                x = self.edge_to[x].unwrap();
+            }
+
+            path.push(self.origin);
+
+            Some(path)
+        } else {
+            None
+        }
+    }
+
+    fn bfs(&mut self, g: &UGraph, v: usize) {
+        let mut q = LinkedQueue::new();
+
+        q.enqueue(self.origin);
+        self.marked[self.origin] = true;
+
+        while !q.is_empty() {
+            let v = q.dequeue().unwrap();
+
+            for w in g.get_adj_edges(v).iter() {
+                if !self.marked[*w] {
+                    q.enqueue(*w);
+                    self.marked[*w] = true;
+                    self.edge_to[*w] = Some(v);
+                }
+            }
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use crate::algorithms::linked_list_stack::LinkedStack;
-    use crate::algorithms::undirected_graph::{DFSPaths, UGraph};
+    use crate::algorithms::undirected_graph::{BFSPaths, DFSPaths, UGraph};
 
     #[test]
-    fn interface_operations_should_work_as_expected() {
+    fn dfs_interface_operations_should_work_as_expected() {
         let mut g = UGraph::new(10);
 
         g.add_edge(0, 1);
@@ -112,6 +179,45 @@ mod tests {
         g.add_edge(7, 8);
 
         let paths = DFSPaths::new(&g, 0);
+
+        assert!(paths.has_path_to(0));
+        assert!(paths.has_path_to(6));
+        assert!(paths.has_path_to(4));
+        assert!(paths.has_path_to(5));
+        assert!(paths.has_path_to(3));
+
+        let path = paths.path_to(3).unwrap();
+        let mut expected_verts = LinkedStack::<usize>::new();
+
+        expected_verts.push(5);
+        expected_verts.push(4);
+        expected_verts.push(6);
+        expected_verts.push(0);
+
+        for v in path.iter() {
+            println!("{}", v);
+            assert!(*v == expected_verts.pop().unwrap());
+        }
+
+        assert!(!paths.has_path_to(8));
+    }
+
+    #[test]
+    fn bfs_interface_operations_should_work_as_expected() {
+        let mut g = UGraph::new(10);
+
+        g.add_edge(0, 1);
+
+        g.add_edge(0, 2);
+
+        g.add_edge(0, 6);
+        g.add_edge(6, 4);
+        g.add_edge(4, 5);
+        g.add_edge(5, 3);
+
+        g.add_edge(7, 8);
+
+        let paths = BFSPaths::new(&g, 0);
 
         assert!(paths.has_path_to(0));
         assert!(paths.has_path_to(6));
